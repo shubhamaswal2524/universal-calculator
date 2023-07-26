035f932d9bd9420b65ace493b3d9e29be73a3efe
shubhamaswal <shubham.aswal@antiersolutions.com>
2023-07-26 23:03:14 +0530

    changes

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
-//done 1 = both valid contollable numbers
-//done 2 = num1 is e and num 2 is normal
-//done 3 = num1 is normal and num2 is e
-//done 4 = both are decimal but not e
-//done 5 = num1 is decimal and num2 is normal
-//done 6 = num1 is normal and num2 is decimal
-//done 7 = num1 is e and num2 is decimal
-//done 8 = num1 is decimal and num2 is e
-//done 9 = both are e
-//done 10 = invalid number
-//done 50 - num1 is not a number
-//done 51 - num2 is not a number
-//done 52 - num1 and num2 are not a number
-
-//done negative numbers
+// 1 = both valid contollable numbers
+// 2 = num1 is e and num 2 is normal
+// 3 = num1 is normal and num2 is e
+// 4 = both are decimal but not e
+// 5 = num1 is decimal and num2 is normal
+// 6 = num1 is normal and num2 is decimal
+// 7 = num1 is e and num2 is decimal
+// 8 = num1 is decimal and num2 is e
+// 9 = both are e
+// 10 = invalid number
+// 50 - num1 is not a number
+// 51 - num2 is not a number
+// 52 - num1 and num2 are not a number
 
 const toCustomFixed = (num: any, fixed: number) => {
   const re = new RegExp("^-?\\d+(?:.\\d{0," + (fixed || -1) + "})?");
@@ -23,21 +21,10 @@ function isNegative(num: any) {
   if (Math.sign(num) === -1) {
     return true;
   }
-
   return false;
 }
 
 const checkNumberTypes = (newNum1: any, newNum2: any) => {
-  // let arr1: any = [];
-  // let arr2: any = [];
-  // for (let i = 0; i < newNum1?.length; i++) {
-  //   if (!Number(newNum1[i])) arr1.push(newNum1[i]);
-  // }
-  // for (let i = 0; i < newNum2?.length; i++) {
-  //   if (!Number(newNum2[i])) arr2.push(newNum2[i]);
-  // }
-  // console.log("first", arr1, arr2);
-  //  need to add reverse condition
   if (isNaN(newNum1)) return 50;
   else if (isNaN(newNum2)) return 51;
   else if (isNaN(newNum1) && isNaN(newNum2)) return 52;
@@ -131,8 +118,6 @@ const decimalAndECalculation = (newNum1: any, newNum2: any) => {
       newDecimals1 = isENegative
         ? Number(splittedEValue[1]) + Number(eDecimals)
         : 0;
-
-      console.log("weiValue", weiValue, newDecimals1);
     } else {
       weiValue = "1";
       newDecimals1 = isENegative
@@ -214,15 +199,7 @@ const scientificCheck = (newNum1: any, newNum2: any, type: any) => {
     calculatedOutput.decimals1 =
       Number(calculatedOutput.decimals1) + Number(decimalLength);
   }
-  console.log(
-    "secondValuecalculated",
-    Number(firstEValueCalculated?.decimals1),
-    Number(secondValuecalculated?.decimals1),
-    newNum1,
-    newNum2,
-    firstEValueCalculated,
-    secondValuecalculated
-  );
+
   return {
     nume1: calculatedOutput?.nume1,
     nume2: calculatedOutput?.nume2,
@@ -238,8 +215,6 @@ const validateNumbers = (val1: any, val2: any) => {
   let length2 = newNum2?.length;
 
   const validType = checkNumberTypes(newNum1, newNum2);
-  console.log("validType", val1, val2, validType);
-  // const validType = checkNumberTypes(newNum1, newNum2);
 
   switch (validType) {
     case 1:
@@ -249,9 +224,19 @@ const validateNumbers = (val1: any, val2: any) => {
         decimals1: 0,
         decimals2: 0,
       };
+    case 10:
+    case 50:
+    case 51:
+    case 52:
+      return {
+        num1: 0,
+        num2: 0,
+        decimals1: 0,
+        decimals2: 0,
+      };
+
     case 2:
       let case2: any = scientificCheck(newNum1, newNum2, 2);
-      console.log("case2", case2);
       return {
         num1: case2?.nume1,
         num2: case2?.nume2,
@@ -260,7 +245,6 @@ const validateNumbers = (val1: any, val2: any) => {
       };
     case 3:
       let case3: any = scientificCheck(newNum1, newNum2, 3);
-      console.log("case3", case3);
       return {
         num1: case3?.nume1,
         num2: case3?.nume2,
@@ -291,7 +275,6 @@ const validateNumbers = (val1: any, val2: any) => {
       };
     case 7:
       let case7: any = scientificCheck(newNum1, newNum2, 7);
-      console.log("case3", case7);
       return {
         num1: case7?.nume1,
         num2: case7?.nume2,
@@ -300,7 +283,6 @@ const validateNumbers = (val1: any, val2: any) => {
       };
     case 8:
       let case8: any = scientificCheck(newNum1, newNum2, 8);
-      console.log("case3", case8);
       return {
         num1: case8?.nume1,
         num2: case8?.nume2,
@@ -309,7 +291,6 @@ const validateNumbers = (val1: any, val2: any) => {
       };
     case 9:
       let case9: any = scientificCheck(newNum1, newNum2, 9);
-      console.log("case3", case9);
       return {
         num1: case9?.nume1,
         num2: case9?.nume2,
@@ -330,28 +311,7 @@ const multiplicationLogic = (
   let len1;
   len1 = num1?.length;
   len2 = num2?.length;
-  console.log("validateNumbers", num1, num2, decimals1, decimals2);
-
-  // if (num1?.toString().includes(".") && !num1?.toString().includes(".")) {
-  //   newLen = num1?.toString()?.length - num1?.toString().indexOf(".") - 1;
-  //   len2 = Number(num2?.toString().length) - Number(newLen);
-  //   num1 = toCustomFixed(Number(num1) * 10 ** newLen, 0);
-  //   console.log("first", num1, num2, len1, len2, newLen);
-  //   num2 = num2?.toString().slice(0, len2);
-  //   num1 = num1.toString();
-  // } else if (
-  //   !num1?.toString().includes(".") &&
-  //   num2?.toString().includes(".")
-  // ) {
-  //   newLen = num2?.toString()?.length - num2?.toString().indexOf(".") - 1;
-  //   len2 = Number(num1?.toString().length) - Number(newLen);
-  //   num1 = toCustomFixed(Number(num1) * 10 ** newLen, 0);
-  //   console.log("first", num1, num2, len1, len2, newLen);
-  //   num2 = num2?.toString().slice(0, len2);
-  //   num1 = num1.toString();
-  // } else {
-  //   len2 = num2.length;
-  // }
+  if (!len1 || !len2) return 0;
   if (len1 == 0 || len2 == 0) return "0";
 
   // will keep the result number in vector
@@ -416,11 +376,9 @@ const multiplicationLogic = (
   }
   if (decimals1 || decimals2) {
     let finalDecimal = Number(decimals1) + Number(decimals2);
-    // let negativeDigits = s.length + finalDecimal;
-    // negativeDigits = negativeDigits < 0 ? -negativeDigits : negativeDigits;
+
     if (finalDecimal < 0) {
       finalDecimal = finalDecimal < 0 ? -finalDecimal : finalDecimal;
-      console.log("sadasd", s.length, finalDecimal, s);
       if (finalDecimal < s.length) {
         s =
           s.slice(0, s.length - finalDecimal) +
@@ -440,12 +398,10 @@ const multiplicationLogic = (
   }
 
   if (s?.includes(".")) {
-    console.log("uhiuhiu", Number(s?.split(".")[1]));
     if (Number(s?.split(".")[1]) == 0) {
       s = s?.split(".")[0];
     }
   }
-  console.log("firadadast", Number(s?.split(".")[1]));
   if (s?.includes(".") && Number(s?.split(".")[1]) == 0) {
     s = toCustomFixed(s, 2);
   }
@@ -453,11 +409,10 @@ const multiplicationLogic = (
   if (isNegative == 1) {
     s = "-" + s;
   }
-
   return s;
 };
 
-function multiply(val1: any, val2: any) {
+function multiply(val1: string, val2: string) {
   let newVal1: any = val1;
   let newVal2: any = val2;
   let isNegative: any;
@@ -475,7 +430,6 @@ function multiply(val1: any, val2: any) {
     val2 = val2?.split("-")[1];
   }
   if (!newVal1?.includes("-") && !newVal2?.includes("-")) isNegative = 0;
-  console.log("isNegative", val1, val2, isNegative);
 
   const { num1, num2, decimals1, decimals2 }: any = validateNumbers(val1, val2);
 
